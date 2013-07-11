3d126845ff34131c00c7ae40d8705882ed605266
Andy Dufferene <dufferene@gmail.com>
2013-07-11 10:32:55 +0000

    modified web.js also

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var fileTextBuf = new Buffer(8);
+fileTextBuf = fs.readFileSync('index.html');
+var fileText= buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(fileText);
 });
 
 var port = process.env.PORT || 5000;
