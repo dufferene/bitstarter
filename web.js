0973da6c9b4e14ec84c965779e4e4b900e90dcea
Andy Dufferene <dufferene@gmail.com>
2013-07-11 12:51:03 +0000

    modified web.js again - - port problem, deleted lchost.js

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var app = express.createServer(express.logger());
-// var fileTextBuf = new Buffer(8);
-// fileTextBuf = fs.readFileSync('index.html');
-// var fileText= buf.toString();
 app.get('/', function(request, response) {
 var fs = require('fs');
-var buf = new Buffer(256);
-response.send(buf.toString('utf8',fs.readFileSync('index.html')));
+var buf = fs.readFileSync("index.html");
+response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
